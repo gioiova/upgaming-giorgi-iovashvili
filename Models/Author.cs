cf48701431b2f153ec79ae4f521212b4bef1d247
gioiova <iovashvili.gio11@gmail.com>
2025-10-27 19:04:48 +0400

    feat:add data models

---
+++ Models/Author.cs
@@ -0,0 +1,8 @@
+﻿namespace BookApi.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookApi.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public int AuthorID { get; set; }
+        [Required]
+        public int PublicationYear { get; set; }
+    }
+}
