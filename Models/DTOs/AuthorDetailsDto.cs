f61c259cca57fcb9a4f54b7892794faebf6a8f3d
gioiova <iovashvili.gio11@gmail.com>
2025-10-27 19:44:07 +0400

    feat: add DTOs for book and author

---
+++ Models/DTOs/AuthorDetailsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BookApi.Models.DTOs
+{
+    public class AuthorDetailsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<BookDto> Books { get; set; } = new List<BookDto>();
+    }
+}
---
+++ Models/DTOs/BookDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BookApi.Models.DTOs
+{
+    public class BookDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public int PublicationYear { get; set; }
+    }
+}
