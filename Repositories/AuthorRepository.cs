08c2557fea81c02096b30d1b730f033ebede6977
gioiova <iovashvili.gio11@gmail.com>
2025-10-27 20:22:32 +0400

    feat:implement AuthorRepository

---
+++ Repositories/AuthorRepository.cs
@@ -0,0 +1,35 @@
+﻿using BookApi.Interfaces;
+using BookApi.Models.DTOs;
+
+namespace BookApi.Repositories
+{
+    public class AuthorRepository : IAuthor
+    {
+        public AuthorDetailsDto GetAuthorWithBooks(int id)
+        {
+            var author = DataStore.Authors.FirstOrDefault(a => a.ID == id);
+            if(author == null)
+            {
+                return null;
+            }
+
+            var authorBooks = DataStore.Books
+                .Where(b => b.AuthorID == id)
+                .Select(b => new BookDto
+                {
+                    Id = b.ID,
+                    Title = b.Title,
+                    AuthorName = author.Name,
+                    PublicationYear = b.PublicationYear
+                }).ToList();
+
+            var authorDetailsDto = new AuthorDetailsDto
+            {
+                Id = author.ID,
+                Name = author.Name,
+                Books = authorBooks
+            };
+            return authorDetailsDto;
+        }
+    }
+}
