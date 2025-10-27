879d6a13307465f5b73b7e0486cb694f16c21595
gioiova <iovashvili.gio11@gmail.com>
2025-10-27 20:23:42 +0400

    feat:add AuthorController with endpoint

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,28 @@
+﻿using BookApi.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly IAuthor _authorRepository;
+        public AuthorController(IAuthor authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+        [HttpGet("/api/authors/{id}")]
+        public IActionResult GetAuthorWithBooks(int id)
+        {
+            var authorDetails = _authorRepository.GetAuthorWithBooks(id);
+            if (authorDetails == null)
+            {
+                return NotFound($"Author with ID {id} not found.");
+            }
+            return Ok(authorDetails);
+        }
+
+    }
+}
